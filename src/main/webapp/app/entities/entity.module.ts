b6a8f49922f728463c962da8c1c6e81e4eb730ef
Tomasz Sobutka <tomaszsobutka@gmail.com>
2020-04-30 21:23:54 +0200

    Przygotowanie - widok wszystkich ofert w jednej zakładce

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -5,26 +5,9 @@ import { RouterModule } from '@angular/router';
   imports: [
     RouterModule.forChild([
       {
-        path: 'student',
-        loadChildren: () => import('./student/student.module').then(m => m.FirstJhippsterStudentModule)
-      },
-      {
-        path: 'backpack',
-        loadChildren: () => import('./backpack/backpack.module').then(m => m.FirstJhippsterBackpackModule)
-      },
-      {
-        path: 'offer',
-        loadChildren: () => import('./offer/offer.module').then(m => m.FirstJhippsterOfferModule)
-      },
-      {
         path: 'offer-version',
         loadChildren: () => import('./offer-version/offer-version.module').then(m => m.FirstJhippsterOfferVersionModule)
-      },
-      {
-        path: 'image',
-        loadChildren: () => import('./image/image.module').then(m => m.FirstJhippsterImageModule)
       }
-      /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
 })
